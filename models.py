b97cca4e147bf4d339cdfade69111af6bb5b75bf
BASIL AHAMED <sbasil.ahamed@gmail.com>
2023-06-17 19:45:17 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Recipe(db.Model):
-    __tablename__ = 'recipes'
+    __tablename__ = 'recipes' # create database and table to store data
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
